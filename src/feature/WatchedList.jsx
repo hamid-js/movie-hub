e1a1022f72e5c1e27eb136eec2f98cc09e26c1ce
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2023-12-28 19:13:26 +0330

    edit bugs

--- src/feature/WatchedList.jsx
+++ src/feature/WatchedList.jsx
@@ -3,7 +3,7 @@ import  WatchedItem  from "./WatchedItem";
 export default function WatchedList({ watched, onDelete }) {
   return (
     <ul className="list">
-      {watched.map((movie) => (
+      {watched?.map((movie) => (
         <WatchedItem onDelete={onDelete} movie={movie} key={movie.imdbId} />
       ))}
     </ul>
--- src/feature/WatchedSummary.jsx
+++ src/feature/WatchedSummary.jsx
@@ -1,9 +1,9 @@
 import { average } from "../App";
 
 export default function WatchedSummary({ watched }) {
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
+  const avgImdbRating = average(watched?.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched?.map((movie) => movie.userRating));
+  const avgRuntime = average(watched?.map((movie) => movie.runtime));
   return (
     <div className="summary">
       <h2>Movies you watched</h2>
